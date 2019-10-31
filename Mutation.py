c76e4d3485babc32d9ef52c4b67f6c83546ba017
quyencao <boy20195@gmail.com>
2019-10-31 15:40:59 +0700

    new commit

--- Mutation.py
+++ Mutation.py
@@ -1,5 +1,6 @@
 import graphene
-from Todo import CreateTodo
+from Todo import CreateTodo, DeleteTodo
 
 class Mutation(graphene.ObjectType):
     create_todo = CreateTodo.Field()
+    delete_todo = DeleteTodo.Field()
