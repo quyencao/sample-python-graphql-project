d218278035506a1897385d6370793813b39646aa
quyencao <boy20195@gmail.com>
2019-10-31 17:40:41 +0700

    new commit

--- Query.py
+++ Query.py
@@ -4,12 +4,12 @@ from db import db
 
 class Query(graphene.ObjectType):
     getTodos = graphene.List(Todo)
-    getTodo = graphene.Field(Todo, id=graphene.ID())
+    # getTodo = graphene.Field(Todo, id=graphene.ID())
 
     def resolve_getTodos(self, info):
         todos = db.getTable("todosTable").getRecords({})
         return todos
 
-    def resolve_getTodo(self, info, id):
-        todo = db.getTable("todosTable").getRecordById(id)
-        return todo
+    # def resolve_getTodo(self, info, id):
+    #     todo = db.getTable("todosTable").getRecordById(id)
+    #     return todo
